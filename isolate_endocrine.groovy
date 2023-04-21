cdc846b374921e7decccc3559add45c4a17a6963
mguyot1 <73506962+mguyot1@users.noreply.github.com>
2023-04-20 21:13:30 -0400

    Update isolate_endocrine.groovy

--- isolate_endocrine.groovy
+++ isolate_endocrine.groovy
@@ -13,35 +13,3 @@ clearSelectedObjects(true);
 
 selectObjectsByClassification("Not Stained");
 clearSelectedObjects(true);
-
-
-
-/*
-setOfCellsToRemove = getCellObjects().findAll{it.getPathClass() != null}
-remove(setOfCellsToRemove)
-//run second classifier on remaining cells
-addObjects(setOfCellsToRemove)
-
-
-runObjectClassifier("Classifier 1", "Classifier 2", "Classifier 3")
-
-def classifiers = [
-    "Classifier 1",
-    "Classifier 2"
-]
-
-// Reset existing classifications
-resetDetectionClassifications()
-
-// Loop through all the classifiers in order
-def imageData = getCurrentImageData()
-def cells = getCellObjects()
-for (name in classifiers) {
-    // Just keep the cells that haven't been classified
-    cells = cells.findAll {it.getPathClass() == null}
-    // Apply the next classifier
-    def classifier = loadObjectClassifier(name)
-    classifier.classifyObjects(imageData, cells, false)
-}
-fireHierarchyUpdate()
-*/
